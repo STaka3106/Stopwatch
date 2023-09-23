f542f8d365693858bf49ac73b9b675e5c43eee8a
Takasato <moopysatomu3106@yahoo.co.jp>
2023-09-23 12:36:50 +0900

    タイムアウトテスト

--- script.js
+++ script.js
@@ -15,7 +15,6 @@
  function displayTime() {
      // 現在の時間
      const currentTime = new Date(Date.now() - startTime + stopTime);
-     console.log(Date.now);
      // 時間、分、秒、小数点秒を頭に0をつけて表示
      const hour = String(currentTime.getHours() - 9).padStart(2, '0');
      const min = String(currentTime.getMinutes()).padStart(2, '0');
@@ -23,7 +22,7 @@
      const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
 
      time.textContent = `${hour}:${min}:${sec}.${ms}`;
-     timeoutID = setTimeout(displayTime, 10);
+     timeoutID = setTimeout(displayTime, 100000);
  }
 
  // スタートボタンクリック時、時間進行
